aac157bba615c85950b8b42417429ed1ddc8f673
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-27 18:02:31 -0500

    Import and add About component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <About />
     </>
   );
 }
