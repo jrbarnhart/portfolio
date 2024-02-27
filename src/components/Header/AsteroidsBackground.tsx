1c865e985f49776f29abc704354cb03ef1f8df75
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-27 17:47:35 -0500

    Add basic component files

---
+++ src/components/Header/AsteroidsBackground.tsx
@@ -0,0 +1,5 @@
+const AsteroidsBackground = () => {
+  return <p>This is the header background game.</p>;
+};
+
+export default AsteroidsBackground;
---
+++ src/components/Header/Nav.tsx
@@ -0,0 +1,22 @@
+const Nav = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <p>About</p>
+        </li>
+        <li>
+          <p>Skills</p>
+        </li>
+        <li>
+          <p>Projects</p>
+        </li>
+        <li>
+          <p>Contact</p>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Nav;
