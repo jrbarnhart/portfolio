7e7df0e97caf09c7cb8ca658702729f6c5b9a5ba
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-27 17:41:16 -0500

    Add basic component file

---
+++ src/components/Header/CTAButton.tsx
@@ -0,0 +1,5 @@
+const CTAButton = () => {
+  return <button>Let&apos;s Chat!</button>;
+};
+
+export default CTAButton;
---
+++ src/components/Header/TitleCard.tsx
@@ -0,0 +1,13 @@
+import CTAButton from "./CTAButton";
+
+const TitleCard = () => {
+  return (
+    <>
+      <p>Hi! My name is</p>
+      <p>Joshua Barnhart</p>
+      <CTAButton />
+    </>
+  );
+};
+
+export default TitleCard;
