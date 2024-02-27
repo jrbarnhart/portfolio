dbb5053676cd48faf9c0800e9320c6cea1096da0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-27 18:36:48 -0500

    Initialization of darkmode handled by index js now

--- src/components/Header/DarkModeButton.tsx
+++ src/components/Header/DarkModeButton.tsx
@@ -1,35 +1,13 @@
 // This component toggles the "dark" class on the html element, implementing
 // the Tailwind CSS selector dark mode strategy. https://tailwindcss.com/docs/dark-mode
 
-import { useEffect } from "react";
-
 const DarkModeButton = () => {
-  // Set initial darkmode preference
-  useEffect(() => {
-    const storedDarkmodePref = localStorage.getItem("darkmode");
-
-    // No stored preference for darkmode found
-    if (!storedDarkmodePref) {
-      const sysDarkmodePref = window.matchMedia("(prefers-color-scheme: dark)");
-      if (sysDarkmodePref.matches) {
-        document.body.classList.add("dark");
-      }
-    } else {
-      // Use the stored preference
-      if (storedDarkmodePref === "true") {
-        document.body.classList.add("dark");
-      } else {
-        document.body.classList.remove("dark");
-      }
-    }
-  }, []);
-
   const toggleDarkmode = () => {
-    if (document.body.classList.contains("dark")) {
-      document.body.classList.remove("dark");
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
       localStorage.setItem("darkmode", "false");
     } else {
-      document.body.classList.add("dark");
+      document.documentElement.classList.add("dark");
       localStorage.setItem("darkmode", "true");
     }
   };
