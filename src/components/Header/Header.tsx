6c102079556b91c402305b382033d9bdc7e3e95a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-27 17:34:56 -0500

    Add file for main header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import DarkModeButton from "../DarkModeButton";
+
+const Header = () => {
+  return (
+    <>
+      <p>This is the header background game!</p>
+      <p>This is the title card!</p>
+      <p>This is the nav!</p>
+      <DarkModeButton />
+    </>
+  );
+};
+
+export default Header;
