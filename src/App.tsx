5a63bcbb574dbb5ad182dd6e775cc567f2c2b731
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-27 18:12:07 -0500

    Add Skills component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import About from "./components/About";
 import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <>
       <Header />
       <About />
+      <Skills />
       <Projects />
       <Contact />
     </>
