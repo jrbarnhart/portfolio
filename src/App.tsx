a20d8aa9b16567fe20b5bf7e8b7cd1546f55cc7a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-27 18:09:52 -0500

    Add remaining Projects and Contact components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import About from "./components/About";
+import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <>
       <Header />
       <About />
+      <Projects />
+      <Contact />
     </>
   );
 }
