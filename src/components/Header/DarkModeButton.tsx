a1816b2f97bb91e25b1057736c440c77cbe5aafa
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-28 17:42:06 -0500

    Set darkmode state when button clicked

--- src/components/Header/DarkModeButton.tsx
+++ src/components/Header/DarkModeButton.tsx
@@ -1,17 +1,28 @@
 // This component toggles the "dark" class on the html element, implementing
 // the Tailwind CSS selector dark mode strategy. https://tailwindcss.com/docs/dark-mode
 
+import { useContext } from "react";
+import DarkmodeContext from "../../contexts/DarkmodeContext";
+
 // Note that the initial theme is set by a small script in the index.html file. This
 // prevents flickering that could occurr if this step were done in a useEffect() here.
 
 const DarkModeButton = () => {
+  const { setDarkmodeState } = useContext(DarkmodeContext);
+
   const toggleDarkmode = () => {
     if (document.documentElement.classList.contains("dark")) {
       document.documentElement.classList.remove("dark");
       localStorage.setItem("darkmode", "false");
+      if (setDarkmodeState) {
+        setDarkmodeState("false");
+      }
     } else {
       document.documentElement.classList.add("dark");
       localStorage.setItem("darkmode", "true");
+      if (setDarkmodeState) {
+        setDarkmodeState("true");
+      }
     }
   };
 
