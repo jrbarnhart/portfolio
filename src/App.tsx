efcc2509a13169444dc70aabc1920c5891782979
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-28 17:41:51 -0500

    Import darkmode context and change toggle to set

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { createContext, useState } from "react";
+import { useState } from "react";
 
 import About from "./components/About";
 import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
-
-interface DarkmodeContext {
-  darkmode: string | null;
-  toggleDarkmodeState: null | (() => void);
-}
-
-const DarkmodeContext = createContext<DarkmodeContext>({
-  darkmode: null,
-  toggleDarkmodeState: null,
-});
+import DarkmodeContext from "./contexts/DarkmodeContext";
 
 function App() {
   const [darkmode, setDarkmode] = useState(localStorage.getItem("darkmode"));
 
-  const toggleDarkmodeState = () => {
-    if (darkmode !== "true") {
-      setDarkmode("true");
-    } else {
-      setDarkmode("false");
-    }
+  const setDarkmodeState = (value: "true" | "false") => {
+    setDarkmode(value);
   };
 
   return (
-    <DarkmodeContext.Provider value={{ darkmode, toggleDarkmodeState }}>
+    <DarkmodeContext.Provider value={{ darkmode, setDarkmodeState }}>
       <Header />
       <About />
       <Skills />
