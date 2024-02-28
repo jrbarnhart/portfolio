89fd20170816624aa7c076aa5294d22cad0bcfa9
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-28 15:49:31 -0500

    Return basic canvas element that draws circles

--- src/components/Header/AsteroidsBackground.tsx
+++ src/components/Header/AsteroidsBackground.tsx
@@ -1,5 +1,38 @@
+// This component is an html canvas that displays the asteroids banner game
+
+import { useEffect, useRef } from "react";
+
 const AsteroidsBackground = () => {
-  return <p>This is the header background game.</p>;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const canvasCtxRef = useRef<CanvasRenderingContext2D | null>(null);
+
+  useEffect(() => {
+    // Initialize canvasCtxRef
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      canvasCtxRef.current = canvas.getContext("2d");
+      // Change the width of the canvas’ inner drawing surface so it’s the same aspect ratio
+      // as the canvas element on the page
+      canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight);
+      const ctx = canvasCtxRef.current;
+
+      // Draw some random circles
+      for (let i = 0; i < 9; i++) {
+        const x = canvasRef.current.width * Math.random();
+        const y = canvasRef.current.height * Math.random();
+        ctx?.beginPath();
+        ctx?.arc(x, y, 40, 0, 2 * Math.PI);
+        ctx?.stroke();
+      }
+    }
+  }, []);
+
+  return (
+    <canvas
+      ref={canvasRef}
+      className="absolute top-0 w-full h-full object-contain"
+    />
+  );
 };
 
 export default AsteroidsBackground;
