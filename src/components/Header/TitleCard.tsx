679ee2a0272fa1f02cb17d8015be5206fc15a73e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-28 16:39:15 -0500

    Add grid layout to title elements

--- src/components/Header/TitleCard.tsx
+++ src/components/Header/TitleCard.tsx
@@ -2,11 +2,13 @@ import CTAButton from "./CTAButton";
 
 const TitleCard = () => {
   return (
-    <>
-      <p>Hi! My name is</p>
-      <p>Joshua Barnhart</p>
+    <div className="grid grid-cols-2 grid-rows-3 w-fit justify-self-center">
+      <p className="col-start-1 self-end">Hi! My name is</p>
+      <h1 className="col-start-1 row-start-2 col-span-2 text-5xl self-center">
+        Joshua Barnhart
+      </h1>
       <CTAButton />
-    </>
+    </div>
   );
 };
 
