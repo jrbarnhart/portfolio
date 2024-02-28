2d4e76650c368b42e877de1ddc0181050487e544
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-28 16:34:42 -0500

    Add basic style to button

--- src/components/Header/CTAButton.tsx
+++ src/components/Header/CTAButton.tsx
@@ -1,5 +1,9 @@
 const CTAButton = () => {
-  return <button className="row-span-2">Let&apos;s Chat!</button>;
+  return (
+    <button className="self-end text-black bg-green-400 border border-white dark:border-black  rounded-md w-fit p-1">
+      Let&apos;s Chat!
+    </button>
+  );
 };
 
 export default CTAButton;
