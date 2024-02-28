e40ffdabf761ff37170ba8533e97037ee44f7983
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-28 16:38:44 -0500

    Move to third grid row

--- src/components/Header/CTAButton.tsx
+++ src/components/Header/CTAButton.tsx
@@ -1,6 +1,6 @@
 const CTAButton = () => {
   return (
-    <button className="self-end text-black bg-green-400 border border-white dark:border-black  rounded-md w-fit p-1">
+    <button className="row-start-3 self-center text-black bg-green-400 border border-white dark:border-black  rounded-md w-fit p-1">
       Let&apos;s Chat!
     </button>
   );
