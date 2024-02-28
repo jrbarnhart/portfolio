7fcdd54faab6b15b7f25040408b251efe4beb310
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-28 17:24:28 -0500

    Add context for reading and toggling darkmode state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
+import { createContext, useState } from "react";
+
 import About from "./components/About";
 import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 
+interface DarkmodeContext {
+  darkmode: string | null;
+  toggleDarkmodeState: null | (() => void);
+}
+
+const DarkmodeContext = createContext<DarkmodeContext>({
+  darkmode: null,
+  toggleDarkmodeState: null,
+});
+
 function App() {
+  const [darkmode, setDarkmode] = useState(localStorage.getItem("darkmode"));
+
+  const toggleDarkmodeState = () => {
+    if (darkmode !== "true") {
+      setDarkmode("true");
+    } else {
+      setDarkmode("false");
+    }
+  };
+
   return (
-    <>
+    <DarkmodeContext.Provider value={{ darkmode, toggleDarkmodeState }}>
       <Header />
       <About />
       <Skills />
       <Projects />
       <Contact />
-    </>
+    </DarkmodeContext.Provider>
   );
 }
 
