0cbefc1a649ba26b9a2ee915684ff10691f221bb
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-01 17:59:34 -0500

    Set width

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,8 +2,11 @@ import Portrait from "./Portrait";
 
 const About = () => {
   return (
-    <div className="grid grid-cols-2">
-      <p>This is some information about me.</p>
+    <div className="grid grid-cols-2 w-[700px]">
+      <p>
+        I create interactive web applications with modern tools like React,
+        Next.js, Express, Mongoose/MongoDB and more.
+      </p>
       <Portrait />
     </div>
   );
