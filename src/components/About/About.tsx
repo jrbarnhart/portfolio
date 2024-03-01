75452e991a4bb6dc41bb839c0ff2fda20210c7d6
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-01 18:16:30 -0500

    Style text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,10 +2,10 @@ import Portrait from "./Portrait";
 
 const About = () => {
   return (
-    <div className="grid grid-cols-2 w-[700px]">
-      <p>
+    <div className="justify-self-center grid grid-cols-2 w-[768px] py-10 px-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl mt-5">
+      <p className="self-center text-2xl px-2 font-semibold">
         I create interactive web applications with modern tools like React,
-        Next.js, Express, Mongoose/MongoDB and more.
+        Next.js, Express, Mongoose / MongoDB and more.
       </p>
       <Portrait />
     </div>
