85a14c5c052a54fad9325475f982ee7d93dbc6eb
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-01 16:55:09 -0500

    Add image and grid layout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,15 @@
+import portraitImage from "../assets/cartoonMe.jpg";
+
 const About = () => {
   return (
-    <>
+    <div className="grid grid-cols-2">
       <p>This is some information about me.</p>
-      <p>This is an image of me.</p>
-    </>
+      <img
+        src={portraitImage}
+        alt="A cartoon portrait of Joshua Barnhart"
+        className="h-96"
+      />
+    </div>
   );
 };
 
