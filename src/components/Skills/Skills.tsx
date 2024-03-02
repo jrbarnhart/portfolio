74e3651d2756ac53acd1bac61b0e9847c9970096
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-02 14:20:18 -0500

    Add simple skills content and style

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,15 +1,49 @@
 const Skills = () => {
   return (
-    <>
-      <p>This is a skill card.</p>
-      <p>This is a skill card.</p>
-      <p>This is a skill card.</p>
-      <p>This is a skill card.</p>
-      <p>This is a mini skill card.</p>
-      <p>This is a mini skill card.</p>
-      <p>This is a mini skill card.</p>
-      <p>This is a mini skill card.</p>
-    </>
+    <div className="justify-self-center w-[768px] grid grid-flow-col p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl">
+      <div>
+        <h2 className="text-2xl border-b-2 border-black dark:border-neutral-50 w-fit">
+          Languages
+        </h2>
+        <ul className="text-lg">
+          <li>Javascript</li>
+          <li>Typescript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="text-2xl border-b-2 border-black dark:border-neutral-50 w-fit">
+          Tools
+        </h2>
+        <ul className="text-lg">
+          <li>
+            <p>Mongo DB / Mongoose</p>
+          </li>
+          <li>
+            <p>Express.js</p>
+          </li>
+          <li>
+            <p>React</p>
+          </li>
+          <li>
+            <p>Node.js</p>
+          </li>
+          <li>
+            <p>Next.js</p>
+          </li>
+          <li>
+            <p>Tailwind CSS</p>
+          </li>
+          <li>
+            <p>Vitest</p>
+          </li>
+          <li>
+            <p>NextAuth.js</p>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
