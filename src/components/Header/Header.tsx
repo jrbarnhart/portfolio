7c8c8dd7b092f5b44763f82ca90b6a27763017d3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-03 16:27:20 -0500

    Adjust height and width

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import TitleCard from "./TitleCard";
 
 const Header = () => {
   return (
-    <div className="sticky top-0 z-[999] w-full h-96 bg-transparent text-neutral-50 dark:text-black grid">
+    <div className="sticky top-0 z-[999] w-full md:h-96 bg-transparent text-neutral-50 dark:text-black grid">
       <AsteroidsBackground />
       <TitleCard />
       <DarkModeButton />
