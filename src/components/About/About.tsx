16e3722b5f103bd08e5306c29bfda847968b9ac1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-03 17:10:14 -0500

    Adjust style and style for md

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,8 +2,8 @@ import Portrait from "./Portrait";
 
 const About = () => {
   return (
-    <div className="justify-self-center grid grid-cols-2 w-full p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl mt-5">
-      <p className="self-center text-2xl font-semibold">
+    <div className="justify-self-center grid gap-y-2 md:grid-cols-2 w-full p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl mt-5">
+      <p className="self-center text-center md:text-left text-2xl font-semibold">
         I create interactive web applications with modern tools like React,
         Next.js, Express, Mongoose / MongoDB and more.
       </p>
