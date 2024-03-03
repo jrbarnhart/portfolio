367818e9e9fd5d98c8544be6e49361a8aecde23e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-03 14:38:02 -0500

    Style project card

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -10,19 +10,28 @@ const ProjectCard = ({
   alt,
 }: ProjectCardProps) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <img src={src} alt={alt} />
-      <a href={demoUrl} target="_blank" rel="noreferrer">
-        Demo
-      </a>
-      <a href={codeUrl} target="_blank" rel="noreferrer">
-        Code
-      </a>
-      {skills.map((skill) => {
-        return <p key={skill}>{skill}</p>;
-      })}
+    <div className="grid grid-cols-2 gap-x-2">
+      <h2 className=" text-2xl">{title}:</h2>
+      <div className="relative grid row-span-3 col-start-2">
+        <img className="rounded-xl" src={src} alt={alt} />
+        <div className="absolute top-0 grid grid-flow-col w-full h-full justify-items-center items-center">
+          <a className="" href={demoUrl} target="_blank" rel="noreferrer">
+            Demo
+          </a>
+          <a href={codeUrl} target="_blank" rel="noreferrer">
+            Code
+          </a>
+        </div>
+      </div>
+      <p className="font-bold">{description}</p>
+      <div>
+        <h3 className="text-lg">Skills & Tech</h3>
+        <div className="flex gap-2 flex-wrap">
+          {skills.map((skill) => {
+            return <p key={skill}>&#x2022;{skill}</p>;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
