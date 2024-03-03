b14f72aa11858134ee560bf4b434cd4c38781930
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-03 14:12:53 -0500

    Style container div and add header

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,7 +3,8 @@ import projectInfo from "./projectInfo";
 
 const Projects = () => {
   return (
-    <div className="justify-self-center grid w-full">
+    <div className="justify-self-center grid gap-y-5 w-full p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl">
+      <h1 className="text-3xl">Projects</h1>
       {projectInfo.map((info) => {
         return <ProjectCard key={info.title} {...info} />;
       })}
