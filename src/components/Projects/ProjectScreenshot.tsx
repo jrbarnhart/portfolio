b4e97280ab40f88286ee0e3684be6d07b887fcae
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-03 15:14:28 -0500

    Add logic for showing or hiding links

--- src/components/Projects/ProjectScreenshot.tsx
+++ src/components/Projects/ProjectScreenshot.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const ProjectScreenshot = ({
   src,
   alt,
@@ -9,14 +11,53 @@ const ProjectScreenshot = ({
   demoUrl: string;
   codeUrl: string;
 }) => {
+  const [linksVisible, setLinksVisible] = useState(false);
+  const visibleClasses =
+    "not-sr-only absolute top-0 grid grid-flow-col w-full h-full justify-items-center items-center";
+  const notVisibleClasses = "sr-only";
+
   return (
-    <div className="relative grid row-span-3 col-start-2">
+    <div
+      onClick={() => {
+        setLinksVisible(!linksVisible);
+      }}
+      onMouseEnter={() => {
+        setLinksVisible(true);
+      }}
+      onMouseLeave={() => {
+        setLinksVisible(false);
+      }}
+      className="relative grid row-span-3 col-start-2"
+    >
       <img className="rounded-xl" src={src} alt={alt} />
-      <div className="absolute top-0 grid grid-flow-col w-full h-full justify-items-center items-center">
-        <a className="" href={demoUrl} target="_blank" rel="noreferrer">
+      <div className={linksVisible ? visibleClasses : notVisibleClasses}>
+        <div className="bg-neutral-600 opacity-50 rounded-xl h-full w-full absolute top-0" />
+        <a
+          className="bg-neutral-600 rounded-lg px-2 border-black border-2 py-1 text-2xl z-10"
+          href={demoUrl}
+          target="_blank"
+          rel="noreferrer"
+          onFocus={() => {
+            setLinksVisible(true);
+          }}
+          onBlur={() => {
+            setLinksVisible(false);
+          }}
+        >
           Demo
         </a>
-        <a href={codeUrl} target="_blank" rel="noreferrer">
+        <a
+          className="bg-neutral-600 rounded-lg px-2 border-black border-2 py-1 text-2xl z-10"
+          href={codeUrl}
+          target="_blank"
+          rel="noreferrer"
+          onFocus={() => {
+            setLinksVisible(true);
+          }}
+          onBlur={() => {
+            setLinksVisible(false);
+          }}
+        >
           Code
         </a>
       </div>
