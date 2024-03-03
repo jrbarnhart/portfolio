03b2c17d9bfb49136fae0ce33973635f4be391ca
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-03 13:23:48 -0500

    Add other projects info

--- src/components/Projects/projectInfo.ts
+++ src/components/Projects/projectInfo.ts
@@ -2,6 +2,8 @@ import { ProjectCardProps } from "./types";
 
 // Project screenshots
 import memoryCardsScreen from "../../assets/memoryCardsSplashLQ.jpg";
+import battleshipScreen from "../../assets/battleshipSplashLQ.jpg";
+import cvScreen from "../../assets/cvCreatorSplashLQ.jpg";
 
 const projectInfo: ProjectCardProps[] = [
   {
@@ -13,6 +15,26 @@ const projectInfo: ProjectCardProps[] = [
     codeUrl: "https://github.com/xincryptedx/memory-cards",
     src: memoryCardsScreen,
     alt: "Screenshot of Memory Cards application",
+  },
+  {
+    title: "Battle-Mech",
+    description:
+      "Play a Battleship-styled game against an intelligent AI opponent.",
+    skills: ["React", "Jest", "Javascript", "Test Driven Design"],
+    demoUrl: "https://xincryptedx.github.io/battleship/",
+    codeUrl: "https://github.com/xincryptedx/battleship",
+    src: battleshipScreen,
+    alt: "Screenshot of Battle-Mech application",
+  },
+  {
+    title: "CV Creator",
+    description:
+      "Add your information to a form to have a CV automatically generated for saving or printing.",
+    skills: ["React", "React State & Props", "Vite"],
+    demoUrl: "https://cv-creator-git-main-xincryptedx.vercel.app/",
+    codeUrl: "https://github.com/xincryptedx/cv-creator",
+    src: cvScreen,
+    alt: "Screenshot of Battle-Mech application",
   },
 ];
 
