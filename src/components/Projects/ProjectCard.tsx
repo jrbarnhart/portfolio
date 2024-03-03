dd53aa9ebf97c8a7ba4d4d685d5dbfd50a651842
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-03 14:47:19 -0500

    Implement new ProjectScreenshot component

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import ProjectScreenshot from "./ProjectScreenshot";
 import { ProjectCardProps } from "./types";
 
 const ProjectCard = ({
@@ -12,17 +13,12 @@ const ProjectCard = ({
   return (
     <div className="grid grid-cols-2 gap-x-2">
       <h2 className=" text-2xl">{title}:</h2>
-      <div className="relative grid row-span-3 col-start-2">
-        <img className="rounded-xl" src={src} alt={alt} />
-        <div className="absolute top-0 grid grid-flow-col w-full h-full justify-items-center items-center">
-          <a className="" href={demoUrl} target="_blank" rel="noreferrer">
-            Demo
-          </a>
-          <a href={codeUrl} target="_blank" rel="noreferrer">
-            Code
-          </a>
-        </div>
-      </div>
+      <ProjectScreenshot
+        src={src}
+        alt={alt}
+        demoUrl={demoUrl}
+        codeUrl={codeUrl}
+      />
       <p className="font-bold">{description}</p>
       <div>
         <h3 className="text-lg">Skills & Tech</h3>
