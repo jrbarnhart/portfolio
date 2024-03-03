3572a2a24a18b17ba05cc90ddffe4c6f6ad2632e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-03 12:08:20 -0500

    Add title and skills

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,16 +1,24 @@
 const ProjectCard = ({
   src,
   alt,
+  title,
   description,
+  skills,
 }: {
   src: string;
   alt: string;
+  title: string;
   description: string;
+  skills: string[];
 }) => {
   return (
     <div>
+      <h2>{title}</h2>
       <p>{description}</p>
       <img src={src} alt={alt} />
+      {skills.map((skill) => {
+        return <p key={skill}>{skill}</p>;
+      })}
     </div>
   );
 };
