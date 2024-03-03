171ee4f3cf0d7dbd7cf670acf892d2854cd4ce6e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-03 16:57:38 -0500

    Adjust style based on screen size

--- src/components/Header/TitleCard.tsx
+++ src/components/Header/TitleCard.tsx
@@ -3,9 +3,9 @@ import Nav from "./Nav";
 
 const TitleCard = () => {
   return (
-    <div className="grid grid-cols-2 grid-rows-4 w-full justify-self-center px-5">
+    <div className="grid grid-cols-2 grid-rows-4 w-full max-w-[768px] h-fit md:h-full justify-self-center px-5">
       <p className="col-start-1 col-span-full self-end">Hi! My name is</p>
-      <h1 className="col-start-1 row-start-2 col-span-full text-5xl self-center">
+      <h1 className="col-start-1 row-start-2 col-span-full text-4xl md:text-5xl self-center">
         Joshua Barnhart
       </h1>
       <CTAButton />
