67daf03a7492921c9276da0c8a29252415c2a906
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-05 17:04:53 -0500

    Remove Darkmode button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,11 @@
-import DarkModeButton from "./DarkModeButton";
 import AsteroidsBackground from "./AsteroidsBackground";
 import TitleCard from "./TitleCard";
 
 const Header = () => {
   return (
-    <div className="sticky top-0 z-[999] w-full md:h-96 bg-transparent text-neutral-50 dark:text-black grid">
+    <div className="relative z-[999] w-full md:h-96 bg-transparent text-neutral-50 dark:text-black grid">
       <AsteroidsBackground />
       <TitleCard />
-      <DarkModeButton />
     </div>
   );
 };
