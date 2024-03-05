1f89c35b533bdf1188cceb31d61b676b09ddf333
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-05 18:00:38 -0500

    Remove unneeded z rule

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import TitleCard from "./TitleCard";
 
 const Header = () => {
   return (
-    <div className="relative z-[999] w-full md:h-96 bg-transparent text-neutral-50 dark:text-black grid">
+    <div className="relative w-full md:h-96 bg-transparent text-neutral-50 dark:text-black grid">
       <AsteroidsBackground />
       <TitleCard />
     </div>
