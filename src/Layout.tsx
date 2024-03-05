368f918fb5f09d314f1c7d428d7740ba380cdd73
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-05 17:04:41 -0500

    Add nav to layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,14 @@
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 
 const MainLayout = () => {
   return (
     <div className="grid">
+      <Nav />
       <Header />
       <main className="justify-self-center grid gap-y-5  w-screen md:w-[768px]">
         <About />
