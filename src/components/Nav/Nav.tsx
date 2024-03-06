f155856fc68e16d3d2b561ef768877b16eb3d672
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-05 19:10:13 -0500

    Adjust grid layout for smaller than lg screens

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,8 +2,8 @@ import DarkModeButton from "./DarkModeButton";
 
 const Nav = () => {
   return (
-    <nav className="sticky z-[999] top-0 grid grid-cols-3 w-full bg-neutral-50 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50 ">
-      <ul className="justify-self-center col-start-2 flex justify-between w-full max-w-[768px] p-5">
+    <nav className="sticky z-[999] top-0 grid lg:grid-cols-3 w-full bg-neutral-50 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50 ">
+      <ul className="justify-self-center lg:col-start-2 flex justify-between md:w-[768px] p-5">
         <li>
           <a href="#about">About</a>
         </li>
