13bcf72ea492afcd203e87ea913c8a335f513b0a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-06 14:18:08 -0500

    Move id to correct component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,7 +3,10 @@ import projectInfo from "./projectInfo";
 
 const Projects = () => {
   return (
-    <div className="justify-self-center grid gap-y-5 w-full p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl">
+    <div
+      id="projects"
+      className="justify-self-center grid gap-y-5 w-full p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl"
+    >
       <h1 className="text-3xl text-center md:text-left">Projects</h1>
       {projectInfo.map((info) => {
         return <ProjectCard key={info.title} {...info} />;
