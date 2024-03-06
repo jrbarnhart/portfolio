b721d999c328b035f74c63207984bd48814af81f
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-06 14:32:08 -0500

    Implement refactored NavLink with offset

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,13 +6,19 @@ const Nav = () => {
     <nav className="sticky z-[999] top-0 grid grid-cols-[1fr_min-content] md:grid-cols-[1fr_min-content_1fr] items-center bg-neutral-50 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50 ">
       <ul className="md:col-start-2 justify-self-center flex justify-between w-full md:w-[768px] p-5">
         <li>
-          <NavLink id="about">About</NavLink>
+          <NavLink id="about" offset={75}>
+            About
+          </NavLink>
         </li>
         <li>
-          <NavLink id="projects">Projects</NavLink>
+          <NavLink id="projects" offset={75}>
+            Projects
+          </NavLink>
         </li>
         <li>
-          <NavLink id="contact">Contact</NavLink>
+          <NavLink id="contact" offset={10}>
+            Contact
+          </NavLink>
         </li>
       </ul>
       <DarkModeButton />
