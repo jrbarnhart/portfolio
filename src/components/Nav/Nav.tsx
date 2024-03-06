aeecb25d5b71cb475a6d3626bd2f1008c6df8d7e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-06 16:14:30 -0500

    Adjust the grid layoutf for md

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,8 +3,8 @@ import DarkModeButton from "./DarkModeButton";
 
 const Nav = () => {
   return (
-    <nav className="sticky z-[999] top-0 grid grid-cols-[1fr_min-content] md:grid-cols-[1fr_min-content_1fr] items-center bg-neutral-50 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50 ">
-      <ul className="md:col-start-2 justify-self-center flex justify-between w-full md:w-[768px] p-5">
+    <nav className="sticky z-[999] top-0 grid grid-cols-[1fr_min-content] md:grid-cols-[1fr_5fr_1fr] items-center bg-neutral-50 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50 ">
+      <ul className="md:col-start-2 justify-self-center flex justify-between w-full max-w-[768px] p-5">
         <li>
           <NavLink id="about" offset={75}>
             About
