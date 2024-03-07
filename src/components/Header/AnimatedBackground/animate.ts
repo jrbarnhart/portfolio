e91370eb391ed59f83aa4ef3645e6b4516d98723
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-07 16:30:46 -0500

    Add file for animation function

---
+++ src/components/Header/AnimatedBackground/animate.ts
@@ -0,0 +1,12 @@
+const animate = () => {
+  // For each particle
+  // Calc force v2
+  // Move particle using force
+  // Reverse velocity on boundary collision
+  // Then
+  // Erase canvas
+  // Draw particles
+  // Recursively call fn
+};
+
+export default animate;
