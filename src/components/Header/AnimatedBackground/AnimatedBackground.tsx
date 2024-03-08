fa719ca10f9f4d667278d4ee91ea2443afca841a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-03-07 19:35:56 -0500

    Separate useEffect logic

--- src/components/Header/AnimatedBackground/AnimatedBackground.tsx
+++ src/components/Header/AnimatedBackground/AnimatedBackground.tsx
@@ -1,6 +1,6 @@
 // This component is an html canvas that displays the asteroids banner game
 
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import DarkmodeContext from "../../../contexts/DarkmodeContext";
 import createParticle, { ParticleInterface } from "./createParticle";
 import animate from "./animate";
@@ -14,10 +14,10 @@ const AnimatedBackground = () => {
 
   const { darkmode } = useContext(DarkmodeContext);
 
-  const particles: ParticleInterface[] = [];
+  const [particles, setParticles] = useState<ParticleInterface[]>([]);
 
+  // Initialize the canvas if it exists and is not yet initialized
   useEffect(() => {
-    // Initialize the canvas if it exists and is not yet initialized
     if (canvasRef.current && !canvasInitialized.current) {
       const canvas = canvasRef.current;
       canvasCtxRef.current = canvas.getContext("2d");
@@ -28,31 +28,39 @@ const AnimatedBackground = () => {
       canvasInitialized.current = true;
       console.log("Canvas initialized.");
     }
+  }, []);
 
-    // Initialize particles if canvas is initialized and particles not yet initialized
+  // Initialize particles if canvas is initialized and particles not yet initialized
+  useEffect(() => {
     if (
       canvasRef.current &&
       canvasInitialized.current &&
       !particlesInitialized.current
     ) {
       // Initialize particles
-      for (let i = 0; i < 1000; i++) {
+      const initialParticles = [];
+      for (let i = 0; i < 5; i++) {
         const newParticle = createParticle({
           x: canvasRef.current.width * Math.random(),
           y: canvasRef.current.height * Math.random(),
           v: { x: 2 * Math.random(), y: 2 * Math.random() },
           mass: 10,
+          color: darkmode === "true" ? "black" : "white",
         });
 
-        particles.push(newParticle);
+        initialParticles.push(newParticle);
+        console.log(initialParticles);
       }
+
+      setParticles(initialParticles);
 
       particlesInitialized.current = true;
       console.log("Particles initialized.");
-      console.log(particles);
     }
+  }, [darkmode]);
 
-    // Start animation if canvas and particles are initialized and animation has not already started
+  // Start animation if canvas and particles are initialized and animation has not already started
+  useEffect(() => {
     if (
       canvasRef.current &&
       canvasCtxRef.current &&
@@ -63,6 +71,7 @@ const AnimatedBackground = () => {
       // Start the animation
       animate({
         particles,
+        setParticles,
         canvasX: canvasRef.current.width,
         canvasY: canvasRef.current.height,
         ctx: canvasCtxRef.current,
@@ -71,46 +80,6 @@ const AnimatedBackground = () => {
       console.log("Animation started.");
     }
   }, [particles]);
-
-  /*  const drawCircles = useCallback((color: string) => {
-    if (canvasCtxRef.current && canvasRef.current) {
-      const ctx = canvasCtxRef.current;
-
-      // Clear the canvas
-      ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-
-      // Draw some random circles
-      for (let i = 0; i < 9; i++) {
-        const x = canvasRef.current.width * Math.random();
-        const y = canvasRef.current.height * Math.random();
-        ctx.beginPath();
-        ctx.arc(x, y, 40, 0, 2 * Math.PI);
-        ctx.strokeStyle = color;
-        ctx.stroke();
-      }
-    }
-  }, []);
-
-  // Initialize canvas
-  useEffect(() => {
-    if (canvasRef.current && !canvasInitialized.current) {
-      const canvas = canvasRef.current;
-      canvasCtxRef.current = canvas.getContext("2d");
-      // Change the width of the canvas’ inner drawing surface so it’s the same aspect ratio
-      // as the canvas element on the page
-      canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight);
-
-      canvasInitialized.current = true;
-    }
-  }, []);
-
-  // Draw circles
-  useEffect(() => {
-    const color = darkmode === "true" ? "black" : "white";
-    if (darkmode && canvasInitialized.current) {
-      drawCircles(color);
-    }
-  }, [darkmode, drawCircles]); */
 
   return (
     <canvas
