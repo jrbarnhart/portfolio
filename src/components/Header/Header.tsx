cf71ff5f502e6925d4c2a0d838a5f56da6f9215c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-19 16:49:55 -0400

    Remove animated background component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,8 @@
-import AnimatedBackground from "./AnimatedBackground/AnimatedBackground";
 import TitleCard from "./TitleCard";
 
 const Header = () => {
   return (
-    <div className="relative w-full md:h-96 bg-transparent text-neutral-50 dark:text-black grid">
-      <AnimatedBackground />
+    <div className="relative w-full md:h-96 bg-neutral-950 dark:bg-neutral-50 text-neutral-50 dark:text-black grid">
       <TitleCard />
     </div>
   );
