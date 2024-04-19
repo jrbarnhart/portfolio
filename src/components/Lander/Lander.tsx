966f2f3b595b1c8be2cc30dfe2c4f0fc6b0f5952
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-19 17:50:44 -0400

    Remove nav

--- src/components/Lander/Lander.tsx
+++ src/components/Lander/Lander.tsx
@@ -2,7 +2,7 @@ import TitleCard from "./TitleCard";
 
 const Lander = () => {
   return (
-    <div className="relative w-full md:h-96 bg-neutral-950 dark:bg-neutral-50 text-neutral-50 dark:text-black grid">
+    <div className="w-full h-screen bg-neutral-950 dark:bg-neutral-50 text-neutral-50 dark:text-black grid">
       <TitleCard />
     </div>
   );
