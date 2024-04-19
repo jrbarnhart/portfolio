f16eb377831a7844ab91a635b28b1ca7feab1374
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-19 17:51:08 -0400

    Add nav after lander

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,13 +1,15 @@
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Lander from "./components/Lander/Lander";
+import Nav from "./components/Nav/Nav";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
 
 const MainLayout = () => {
   return (
-    <main className="justify-self-center grid gap-y-5 h-screen w-full max-w-[768px] overflow-y-auto">
+    <main className="justify-self-center grid gap-y-5 h-screen w-screen max-w-[768px] overflow-y-auto">
       <Lander />
+      <Nav />
       <About />
       <Skills />
       <Projects />
