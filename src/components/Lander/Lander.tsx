0402041ed66b5b35c440f4e5591689f9033cd8de
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-19 19:08:16 -0400

    Adjust row size

--- src/components/Lander/Lander.tsx
+++ src/components/Lander/Lander.tsx
@@ -1,9 +1,11 @@
+import FunButton from "./FunButton";
 import TitleCard from "./TitleCard";
 
 const Lander = () => {
   return (
-    <div className="grid grid-rows-3 h-screen w-full bg-transparent text-neutral-950 dark:text-neutral-50">
+    <div className="grid grid-rows-[repeat(3,_min-content)] h-screen w-full bg-transparent text-neutral-950 dark:text-neutral-50">
       <TitleCard />
+      <FunButton />
     </div>
   );
 };
