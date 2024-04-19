6949ca38f59c9bed3111c42be2774966f9a90563
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-19 18:20:31 -0400

    Adjust colors

--- src/components/Lander/Lander.tsx
+++ src/components/Lander/Lander.tsx
@@ -2,7 +2,7 @@ import TitleCard from "./TitleCard";
 
 const Lander = () => {
   return (
-    <div className="h-screen bg-neutral-950 dark:bg-neutral-50 text-neutral-50 dark:text-black grid">
+    <div className="h-screen bg-transparent text-neutral-950 dark:text-neutral-50 grid">
       <TitleCard />
     </div>
   );
