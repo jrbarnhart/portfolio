18008c45c3163ccca1f08ba9698618d4d28936f0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-19 19:12:49 -0400

    Wrap fun button in div to position it

--- src/components/Lander/Lander.tsx
+++ src/components/Lander/Lander.tsx
@@ -5,7 +5,9 @@ const Lander = () => {
   return (
     <div className="grid grid-rows-[repeat(3,_min-content)] h-screen w-full bg-transparent text-neutral-950 dark:text-neutral-50">
       <TitleCard />
-      <FunButton />
+      <div className="grid content-center justify-center py-12">
+        <FunButton />
+      </div>
     </div>
   );
 };
