3c474b34c92da5766e8c39237f38b2c05d8def8a
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-19 18:10:20 -0400

    Set height to 100vh

--- src/components/Lander/Lander.tsx
+++ src/components/Lander/Lander.tsx
@@ -2,7 +2,7 @@ import TitleCard from "./TitleCard";
 
 const Lander = () => {
   return (
-    <div className="w-full h-screen bg-neutral-950 dark:bg-neutral-50 text-neutral-50 dark:text-black grid">
+    <div className="h-screen bg-neutral-950 dark:bg-neutral-50 text-neutral-50 dark:text-black grid">
       <TitleCard />
     </div>
   );
