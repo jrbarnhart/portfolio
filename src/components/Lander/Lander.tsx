e1fe894a3fbb96bcebd0420c49074b8cdfdb605b
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-19 21:34:23 -0400

    Add scroll info p

--- src/components/Lander/Lander.tsx
+++ src/components/Lander/Lander.tsx
@@ -3,10 +3,15 @@ import TitleCard from "./TitleCard";
 
 const Lander = () => {
   return (
-    <div className="grid grid-rows-[repeat(3,_min-content)] h-screen w-full bg-transparent text-neutral-950 dark:text-neutral-50">
+    <div className="grid grid-rows-[min-content_minmax(min-content,_1fr)_1fr] h-screen w-full bg-transparent text-neutral-950 dark:text-neutral-50">
       <TitleCard />
       <div className="grid content-center justify-center py-12">
         <FunButton />
+      </div>
+      <div className="grid items-end">
+        <p className="w-full border-t-2 border-b-2 border-green-500 text-xl text-center mb-12">
+          &darr;Scroll for formal experience&darr;
+        </p>
       </div>
     </div>
   );
