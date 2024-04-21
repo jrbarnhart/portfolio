7a1ae63c43c0f5b6de1f795c6755be9b9c7281a3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-21 19:47:53 -0400

    Adjust info bar position

--- src/components/Lander/Lander.tsx
+++ src/components/Lander/Lander.tsx
@@ -3,13 +3,13 @@ import TitleCard from "./TitleCard";
 
 const Lander = () => {
   return (
-    <div className="grid grid-rows-[min-content_minmax(min-content,_1fr)_1fr] h-screen w-full bg-transparent text-neutral-950 dark:text-neutral-50">
+    <div className="grid grid-rows-[min-content_repeat(2,_minmax(min-content,_1fr))] h-screen w-full bg-transparent text-neutral-950 dark:text-neutral-50">
       <TitleCard />
       <div className="grid content-center justify-center pt-12">
         <FunButton />
       </div>
-      <div className="grid items-end">
-        <p className="w-full border-t-2 border-b-2 border-green-500 text-xl text-center mb-12">
+      <div className="grid items-start pt-10">
+        <p className="w-full border-t-2 border-b-2 border-green-500 text-xl text-center">
           &darr;Scroll for formal experience&darr;
         </p>
       </div>
