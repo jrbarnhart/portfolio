c6bd16233e9ed0a8c8c05002e9ffbbae9ff40545
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-21 20:45:44 -0400

    Make reactive for md sized screens

--- src/components/Lander/FunButton.tsx
+++ src/components/Lander/FunButton.tsx
@@ -1,8 +1,8 @@
 const FunButton = () => {
   return (
     <button className="grid justify-items-center">
-      <div className="grid grid-rows-[min-content,_1fr] w-36 h-28 bg-neutral-300 border-2 border-black rounded-md">
-        <div className="relative w-28 aspect-[4_/_3] bg-black justify-self-center mt-2 mb-[5px] grid items-center rounded-md">
+      <div className="grid grid-rows-[min-content,_1fr] w-36 md:w-56 aspect-[9_/_7] bg-neutral-300 border-2 border-black rounded-md">
+        <div className="relative w-[80%] aspect-[4_/_3] bg-black justify-self-center mt-2 md:mt-4 mb-[5px] grid items-center rounded-md">
           <div
             className="absolute top-0 left-0 w-full h-full border-2 border-black rounded-md shadow-[inset_0_0_20px_rgba(255,_255,_255,_0.25)]"
             style={{
@@ -15,13 +15,13 @@ const FunButton = () => {
           </p>
         </div>
         <div className="relative h-full border-t-2 border-neutral-700">
-          <div className="absolute right-2 w-5 h-full border-l-2 border-r-2 border-neutral-700 grid items-center justify-end">
-            <div className="h-[6px] w-[6px] rounded-full bg-green-500 mr-[2px] border-neutral-700 border-[1px]" />
+          <div className="absolute right-2 w-[13%] h-full border-l-2 border-r-2 border-neutral-700 grid items-center justify-end">
+            <div className="w-[6px] md:w-2 aspect-square mr-[2px] md:mr-1 rounded-full bg-green-500 border-neutral-700 border-[1px]" />
           </div>
         </div>
       </div>
-      <div className="w-16 h-3 bg-neutral-300 border-2 border-t-0 border-b-0 border-black rounded-sm" />
-      <div className="w-24 h-2 bg-neutral-300 border-2 border-black rounded-sm" />
+      <div className="w-16 md:w-24 aspect-[16_/_3] bg-neutral-300 border-2 border-t-0 border-b-0 border-black rounded-sm" />
+      <div className="w-24 md:w-36 aspect-[12_/_1] bg-neutral-300 border-2 border-black rounded-sm" />
     </button>
   );
 };
