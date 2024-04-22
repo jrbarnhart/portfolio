969bc51dc461bbb9ee3f6df0a38caef45abfef79
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-22 17:46:11 -0400

    Adjust top margin

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
       id="about"
-      className="justify-self-center grid gap-y-2 md:grid-cols-2 w-full p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl mt-5"
+      className="justify-self-center grid gap-y-2 md:grid-cols-2 p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl mt-12"
     >
       <p className="self-center text-center md:text-left text-2xl font-semibold">
         I create interactive web applications with modern tools like React,
