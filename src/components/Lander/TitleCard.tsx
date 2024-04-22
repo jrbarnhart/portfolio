39b7ff9a1ca9633cf84ca7beb110bf11223c89b1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-21 20:24:55 -0400

    Add max width rule

--- src/components/Lander/TitleCard.tsx
+++ src/components/Lander/TitleCard.tsx
@@ -1,6 +1,6 @@
 const TitleCard = () => {
   return (
-    <div className="grid grid-rows-[min-content_min-content] gap-y-2 w-full h-full justify-self-center px-4 pt-24">
+    <div className="grid grid-rows-[min-content_min-content] gap-y-2 w-full max-w-[540px] h-full justify-self-center px-4 pt-24">
       <p className="self-end text-5xl text-green-500">Hi!</p>
       <p className="row-start-2 text-2xl">
         My name is Josh. This is my full&#x2011;stack portfolio. <br />
