e10a5e0799f1ee6f905d5758e3d5b91d2b24a3a0
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-23 17:59:57 -0400

    Use correct prop name

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,17 +6,17 @@ const Nav = () => {
     <nav className="sticky z-[999] top-0 grid h-20 mt-[-20px] md:mt-[-32px] lg:mt-[-48px] grid-cols-[1fr_min-content] md:grid-cols-[1fr_5fr_1fr] items-center text-neutral-50 bg-zinc-800">
       <ul className="md:col-start-2 justify-self-center flex justify-between w-full max-w-[768px] p-5">
         <li>
-          <NavLink id="about" offset={100}>
+          <NavLink targetId="about" offset={100}>
             About
           </NavLink>
         </li>
         <li>
-          <NavLink id="projects" offset={100}>
+          <NavLink targetId="projects" offset={100}>
             Projects
           </NavLink>
         </li>
         <li>
-          <NavLink id="contact" offset={10}>
+          <NavLink targetId="contact" offset={10}>
             Contact
           </NavLink>
         </li>
