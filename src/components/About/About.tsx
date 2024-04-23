c954c8918eb393f98e605ec7e9771a4ae7b18474
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-23 19:20:41 -0400

    Adjust style and p content

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,16 +4,16 @@ const About = () => {
   return (
     <div
       id="about"
-      className="justify-self-center grid gap-y-2 px-5 py-3 rounded-3xl mt-12"
+      className="justify-self-center grid gap-y-3 px-5 py-3 rounded-3xl mt-12"
     >
-      <h2 className="col-span-full justify-self-center text-2xl font-bold">
+      <h2 className="col-span-full justify-self-center text-3xl font-bold">
         About
       </h2>
       <Portrait />
       <p className="self-center text-center md:text-left text-xl">
         I am a web developer who creates interactive applications with modern
-        tools. I see education as a lifelong process and I try to be always
-        learning something new.
+        tools. I see education as a lifelong process and I am always learning
+        something new.
       </p>
       <p className="self-center text-center md:text-left text-xl">
         Outside of web dev my interests include game development, storytelling
