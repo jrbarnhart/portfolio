eeab03e4c32a02313f13a03518f58d45dad63837
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-23 18:51:43 -0400

    Make default height smaller

--- src/components/About/Portrait.tsx
+++ src/components/About/Portrait.tsx
@@ -6,7 +6,7 @@ const Portrait = () => {
       <img
         src={portraitImage}
         alt="A cartoon portrait of Joshua Barnhart"
-        className="h-96 rounded-3xl blur-none justify-self-center self-center shadow-lg shadow-black"
+        className="h-52 rounded-3xl blur-none justify-self-center self-center shadow-lg shadow-black"
       />
       <div className="absolute bottom-4 left-0 h-fit w-full rounded-3xl bg-neutral-800 opacity-0 group-hover:opacity-90 transition-opacity duration-75">
         <p className=" text-neutral-50 text-center w-full opacity-0 group-hover:opacity-100 transition-opacity duration-75">
