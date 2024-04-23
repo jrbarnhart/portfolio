d1fa3a3b96c99fb9b9d81ad83339039f82174fec
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-23 19:15:01 -0400

    Adjust offset values

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,12 +6,12 @@ const Nav = () => {
     <nav className="sticky z-[999] top-0 grid h-20 mt-[-20px] md:mt-[-32px] lg:mt-[-48px] grid-cols-[1fr_min-content] md:grid-cols-[1fr_5fr_1fr] items-center text-neutral-50 bg-zinc-800">
       <ul className="md:col-start-2 justify-self-center flex justify-between w-full max-w-[768px] p-5">
         <li>
-          <NavLink targetId="about" offset={100}>
+          <NavLink targetId="about" offset={85}>
             About
           </NavLink>
         </li>
         <li>
-          <NavLink targetId="projects" offset={100}>
+          <NavLink targetId="projects" offset={85}>
             Projects
           </NavLink>
         </li>
