6466026005c3088c0159d46c5781a7c87f0289bd
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-23 22:03:59 -0400

    Adjust grid position and size for md screens

--- src/components/About/Portrait.tsx
+++ src/components/About/Portrait.tsx
@@ -2,11 +2,11 @@ import portraitImage from "../../assets/cartoonMe.jpg";
 
 const Portrait = () => {
   return (
-    <div className="relative group h-fit w-fit justify-self-center md:justify-self-end">
+    <div className="relative group h-fit w-fit justify-self-center self-center md:row-span-2">
       <img
         src={portraitImage}
         alt="A cartoon portrait of Joshua Barnhart"
-        className="h-52 rounded-3xl blur-none justify-self-center self-center shadow-lg shadow-black"
+        className="h-52 md:h-64 rounded-3xl blur-none justify-self-center self-center shadow-lg shadow-black"
       />
       <div className="absolute bottom-4 left-0 h-fit w-full rounded-3xl bg-neutral-800 opacity-0 group-hover:opacity-90 transition-opacity duration-75">
         <p className=" text-neutral-50 text-center w-full opacity-0 group-hover:opacity-100 transition-opacity duration-75">
