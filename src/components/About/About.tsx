ab2be50b64f97efe44b71330111cb57d60b53ab7
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-23 21:51:35 -0400

    Adjust header style and add border

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,7 +6,7 @@ const About = () => {
       id="about"
       className="justify-self-center grid gap-y-3 px-5 py-3 rounded-3xl mt-12"
     >
-      <h2 className="col-span-full justify-self-center text-3xl font-bold">
+      <h2 className="col-span-full justify-self-center w-full border-b-2 border-t-2 border-green-500 text-center text-3xl">
         About
       </h2>
       <Portrait />
