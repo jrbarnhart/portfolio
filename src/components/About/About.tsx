13ae63db2001f4d38f58c57bf664c6c3553cc1c8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-23 22:07:52 -0400

    Adjust gap y on md screens

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
       id="about"
-      className="justify-self-center grid gap-y-3 px-5 py-3 rounded-3xl mt-12 md:grid-cols-2"
+      className="justify-self-center grid gap-y-3 md:gap-y-6 px-5 py-3 rounded-3xl mt-12 md:grid-cols-2"
     >
       <h2 className="col-span-full justify-self-center w-full border-b-2 border-t-2 border-green-500 text-center text-3xl">
         About
