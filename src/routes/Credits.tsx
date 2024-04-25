4bd448e988e0060c2b2553694a2ecb5347956076
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-24 23:19:02 -0400

    Add credits route with simple content

---
+++ src/routes/Credits.tsx
@@ -0,0 +1,15 @@
+const Credits = () => {
+  return (
+    <div>
+      <p>
+        JS Icon:{" "}
+        <a href="https://commons.wikimedia.org/wiki/File:Unofficial_JavaScript_logo_2.svg">
+          Christopher Williams
+        </a>
+        , Public domain, via Wikimedia Commons
+      </p>
+    </div>
+  );
+};
+
+export default Credits;
