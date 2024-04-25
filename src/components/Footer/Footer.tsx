b855fa04654d62194534dd9ef033106634bf816c
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-25 19:30:26 -0400

    Add other links to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,13 +3,22 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="grid justify-items-center p-4 md:p-6 w-full bg-zinc-800">
-      <div className="w-full max-w-[1024px]">
+      <div className="grid grid-flow-col justify-items-center w-full max-w-[1024px]">
         <Link
           className="hover:text-green-500 md:text-lg lg:text-xl font-bold w-min"
           to="credits"
         >
           Credits
         </Link>
+        <a
+          className="hover:text-green-500 md:text-lg lg:text-xl font-bold w-min"
+          href="https://github.com/jrbarnhart"
+        >
+          GitHub
+        </a>
+        <p className="hover:text-green-500 md:text-lg lg:text-xl font-bold w-min">
+          LinkedIn(NYI)
+        </p>
       </div>
     </footer>
   );
