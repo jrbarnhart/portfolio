3dbb1b758e6e9b17763b508b432ee0ec98d1be68
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-25 19:37:31 -0400

    Underline links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,18 +5,18 @@ const Footer = () => {
     <footer className="grid justify-items-center p-4 md:p-6 w-full bg-zinc-800">
       <div className="grid grid-flow-col justify-items-center w-full max-w-[1024px]">
         <Link
-          className="hover:text-green-500 md:text-lg lg:text-xl font-bold w-min"
+          className="w-min hover:text-green-500 md:text-lg lg:text-xl font-bold underline"
           to="credits"
         >
           Credits
         </Link>
         <a
-          className="hover:text-green-500 md:text-lg lg:text-xl font-bold w-min"
+          className="w-min hover:text-green-500 md:text-lg lg:text-xl font-bold underline"
           href="https://github.com/jrbarnhart"
         >
           GitHub
         </a>
-        <p className="hover:text-green-500 md:text-lg lg:text-xl font-bold w-min">
+        <p className="w-min hover:text-green-500 md:text-lg lg:text-xl font-bold underline">
           LinkedIn(NYI)
         </p>
       </div>
