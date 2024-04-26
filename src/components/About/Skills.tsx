3d1528a25887b88a8f68510b7fdb76c4922c7739
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-25 21:28:46 -0400

    Adjust grid gaps

--- src/components/About/Skills.tsx
+++ src/components/About/Skills.tsx
@@ -7,7 +7,7 @@ const Skills = () => {
       <h3 className="col-span-full justify-self-center w-min border-b-2 border-green-500 text-center text-2xl font-bold">
         Skills
       </h3>
-      <div className="justify-self-center w-full flex flex-wrap gap-x-8 gap-y-4 justify-center p-5">
+      <div className="justify-self-center w-full flex flex-wrap gap-x-10 gap-y-8 justify-center p-5">
         <SVGLink link="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
           <Icons.js />
         </SVGLink>
