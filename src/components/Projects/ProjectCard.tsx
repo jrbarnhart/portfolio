03c628efafa8f8e36f2bf0fb228f847608048020
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-26 18:22:01 -0400

    Adjust style for md screens

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -11,9 +11,9 @@ const ProjectCard = ({
   alt,
 }: ProjectCardProps) => {
   return (
-    <div className="grid md:grid-cols-2 gap-x-2 gap-y-4 p-3 border-b-2 last:border-b-0 border-green-500">
+    <div className="grid md:grid-cols-2 gap-x-2 md:gap-x-4 gap-y-4 p-3 border-b-2 last:border-b-0 border-green-500">
       <h2 className="col-span-full justify-self-center text-center text-2xl md:text-3xl font-bold">
-        {title}:
+        {title}
       </h2>
       <ProjectScreenshot
         src={src}
@@ -21,10 +21,10 @@ const ProjectCard = ({
         demoUrl={demoUrl}
         codeUrl={codeUrl}
       />
-      <div className="justify-self-center">
-        <p className="text-xl">{description}</p>
+      <div className="justify-self-center md:row-span-2 md:row-start-2 md:self-center">
+        <p className="text-xl md:text-2xl">{description}</p>
         <div>
-          <div className="flex gap-x-2 flex-wrap font-bold text-lg">
+          <div className="flex gap-x-2 flex-wrap font-bold text-lg md:text-xl">
             {skills.map((skill) => {
               return <p key={skill}>&#x2022;{skill}</p>;
             })}
