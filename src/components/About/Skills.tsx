1f47b03fcfc4c810deb20a2e0a20690cbd66e942
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-25 21:25:49 -0400

    Add skill icon links

--- src/components/About/Skills.tsx
+++ src/components/About/Skills.tsx
@@ -3,13 +3,45 @@ import SVGLink from "../SVGLink/SVGLink";
 
 const Skills = () => {
   return (
-    <div className="justify-self-center w-full grid grid-flow-col p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl">
-      <SVGLink link="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
-        <Icons.js />
-      </SVGLink>
-      <SVGLink link="">
-        <Icons.express />
-      </SVGLink>
+    <div className="grid">
+      <h3 className="col-span-full justify-self-center w-min border-b-2 border-green-500 text-center text-2xl font-bold">
+        Skills
+      </h3>
+      <div className="justify-self-center w-full flex flex-wrap gap-x-8 gap-y-4 justify-center p-5">
+        <SVGLink link="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+          <Icons.js />
+        </SVGLink>
+        <SVGLink link="https://developer.mozilla.org/en-US/docs/Web/HTML">
+          <Icons.html />
+        </SVGLink>
+        <SVGLink link="https://developer.mozilla.org/en-US/docs/Web/CSS">
+          <Icons.css />
+        </SVGLink>
+        <SVGLink link="https://tailwindcss.com/">
+          <Icons.tailwind />
+        </SVGLink>
+        <SVGLink link="https://www.typescriptlang.org/">
+          <Icons.ts />
+        </SVGLink>
+        <SVGLink link="https://nodejs.org/">
+          <Icons.node />
+        </SVGLink>
+        <SVGLink link="https://react.dev/">
+          <Icons.react />
+        </SVGLink>
+        <SVGLink link="https://vitejs.dev/">
+          <Icons.vite />
+        </SVGLink>
+        <SVGLink link="https://nextjs.org/">
+          <Icons.next />
+        </SVGLink>
+        <SVGLink link="https://expressjs.com/">
+          <Icons.express />
+        </SVGLink>
+        <SVGLink link="https://www.mongodb.com/">
+          <Icons.mongoDb />
+        </SVGLink>
+      </div>
     </div>
   );
 };
