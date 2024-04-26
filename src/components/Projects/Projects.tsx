ead6adb23bc7a65d18edfa75d669b22f331e791e
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-26 17:53:53 -0400

    Remove bg color and adjust header style

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,9 +5,11 @@ const Projects = () => {
   return (
     <div
       id="projects"
-      className="justify-self-center grid gap-y-5 w-full p-5 bg-neutral-400 dark:bg-neutral-800 rounded-3xl"
+      className="justify-self-center grid gap-y-5 w-full p-5 rounded-3xl"
     >
-      <h1 className="text-3xl text-center md:text-left">Projects</h1>
+      <h1 className="col-span-full justify-self-center w-full border-b-2 border-t-2 border-green-500 text-center text-3xl md:text-4xl font-bold">
+        Projects
+      </h1>
       {projectInfo.map((info) => {
         return <ProjectCard key={info.title} {...info} />;
       })}
