cd2b785d412b94cf14987f100580257bdc0b8a96
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-30 01:31:26 -0400

    Add basic modal styles

--- src/components/FourPicCaptcha/FourPicCaptcha.tsx
+++ src/components/FourPicCaptcha/FourPicCaptcha.tsx
@@ -34,13 +34,27 @@ const FourPicCaptcha = () => {
   }, [shufflePictures]);
 
   return (
-    <div>
-      <p>Human verification.</p>
-      <p>Select the cat.</p>
-      <div>
-        {pictures.map((name, index) => {
-          return <div key={index}>{componentMap[name]}</div>;
-        })}
+    <div className="fixed z-50 top-0 left-0 grid items-center justify-items-center w-screen h-screen bg-zinc-900 bg-opacity-80">
+      <div className="relative grid gap-y-3 w-[300px] bg-zinc-100 dark:bg-zinc-800 rounded-lg p-5 border-2 border-zinc-950">
+        <div className="border-b-2 border-zinc-950">
+          <p className="text-xl">Human verification:</p>
+          <p className="text-3xl font-bold">Select the cat</p>
+          <button className="absolute top-0 right-0 py-5 px-6 font-bold text-xl text-red-500">
+            X
+          </button>
+        </div>
+        <div className="grid grid-cols-2 gap-3">
+          {pictures.map((name) => {
+            return (
+              <div
+                className="p-3 bg-zinc-600 border-2 border-green-500 rounded-md"
+                key={name}
+              >
+                {componentMap[name]}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
