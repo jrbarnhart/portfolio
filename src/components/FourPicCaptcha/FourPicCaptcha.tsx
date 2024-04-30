17c02533ae6c29ccad842b9c7e19664f001bbd13
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-30 00:41:42 -0400

    Shuffle images once on mount

--- src/components/FourPicCaptcha/FourPicCaptcha.tsx
+++ src/components/FourPicCaptcha/FourPicCaptcha.tsx
@@ -1,12 +1,47 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import Icons from "./Icons";
+
 const FourPicCaptcha = () => {
+  const [pictures, setPictures] = useState<string[]>([
+    "cat",
+    "apple",
+    "umbrella",
+    "key",
+  ]);
+  const hasShuffled = useRef<boolean>(false);
+
+  const componentMap: Record<string, React.ReactNode> = {
+    cat: <Icons.cat />,
+    apple: <Icons.apple />,
+    umbrella: <Icons.umbrella />,
+    key: <Icons.key />,
+  };
+
+  const shufflePictures = useCallback(() => {
+    if (hasShuffled.current) return;
+
+    const shuffledPictures = pictures
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+
+    setPictures(shuffledPictures);
+    hasShuffled.current = true;
+  }, [pictures]);
+
+  useEffect(() => {
+    shufflePictures();
+  }, [shufflePictures]);
+
   return (
     <div>
       <p>Human verification.</p>
       <p>Select the cat.</p>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        {pictures.map((name, index) => {
+          return <div key={index}>{componentMap[name]}</div>;
+        })}
+      </div>
     </div>
   );
 };
