fb1e6e308c241a4ad24362256c1d5a4ab578bfb4
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-04-29 23:21:28 -0400

    Add basic four pic captcha skeleton

---
+++ src/components/FourPicCaptcha/FourPicCaptcha.tsx
@@ -0,0 +1,14 @@
+const FourPicCaptcha = () => {
+  return (
+    <div>
+      <p>Human verification.</p>
+      <p>Select the cat.</p>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default FourPicCaptcha;
