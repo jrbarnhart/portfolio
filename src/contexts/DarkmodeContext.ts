284170e624df9738e889ac75b0d823326f92889e
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-09-23 17:22:54 -0400

    Update vite and fix ts errors

--- src/contexts/DarkmodeContext.ts
+++ src/contexts/DarkmodeContext.ts
@@ -1,9 +1,9 @@
 import { createContext } from "react";
 
-interface DarkmodeContext {
+type DarkmodeContext = {
   darkmode: string | null;
   setDarkmodeState: null | ((value: "true" | "false") => void);
-}
+};
 
 const DarkmodeContext = createContext<DarkmodeContext>({
   darkmode: localStorage.getItem("darkmode"),
