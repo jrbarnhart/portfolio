70f77df5b0c4ff0e7b4646800fad719aaffb5d26
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-15 14:10:14 -0400

    Dont wrap text

--- src/components/SkillTag.tsx
+++ src/components/SkillTag.tsx
@@ -7,7 +7,7 @@ const SkillTag = ({
   bgLight?: boolean | undefined;
 }) => {
   return (
-    <div className="w-min h-min p-2 px-4 bg-black dark:bg-white hover:bg-green-500 text-white dark:text-black rounded-lg font-bold">
+    <div className="w-min h-min p-2 px-4 bg-black dark:bg-white hover:bg-green-500 text-white dark:text-black rounded-lg font-bold text-nowrap">
       <a href={link}>{skillName}</a>
     </div>
   );
