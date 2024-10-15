bc4d700315964fd1153ac53e115f7540597e251e
jrbarnhart <151244381+jrbarnhart@users.noreply.github.com>
2024-10-15 13:54:19 -0400

    Add new skills

--- src/components/About/Skills.tsx
+++ src/components/About/Skills.tsx
@@ -22,20 +22,30 @@ const Skills = () => {
           link="https://developer.mozilla.org/en-US/docs/Web/CSS"
         />
         <SkillTag skillName="Tailwind" link="https://tailwindcss.com/" />
-        <SkillTag
-          skillName="Node.js"
-          link="https://nodejs.org/"
-          bgLight={true}
-        />
         <SkillTag skillName="React" link="https://react.dev/" />
-        <SkillTag skillName="Vite" link="https://vitejs.dev/" />
+        <SkillTag skillName="Remix" link="https://remix.run/" />
         <SkillTag
           skillName="Next.js"
           link="https://nextjs.org/"
           bgLight={true}
         />
-        <SkillTag skillName="Express.js" link="https://expressjs.com/" />
+        <SkillTag skillName="Prisma" link="https://www.prisma.io/" />
+        <SkillTag skillName="PostgreSQL" link="https://www.postgresql.org/" />
         <SkillTag skillName="MongoDB" link="https://www.mongodb.com/" />
+        <SkillTag
+          skillName="Node.js"
+          link="https://nodejs.org/"
+          bgLight={true}
+        />
+        <SkillTag skillName="Express.js" link="https://expressjs.com/" />
+        <SkillTag skillName="Vite" link="https://vitejs.dev/" />
+        <SkillTag skillName="Webpack" link="https://webpack.js.org/" />
+        <SkillTag skillName="Docker" link="https://www.docker.com/" />
+        <SkillTag skillName="Nginx" link="https://nginx.org/en/" />
+        <SkillTag
+          skillName="Provisioning"
+          link="https://en.wikipedia.org/wiki/Provisioning_(technology)"
+        />
       </div>
     </div>
   );
