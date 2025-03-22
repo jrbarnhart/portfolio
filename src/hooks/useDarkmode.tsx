bbf9ffc0b7e910312df389266b1d6df45fb30066
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-22 16:52:47 -0400

    Add typed props

--- src/hooks/useDarkmode.tsx
+++ src/hooks/useDarkmode.tsx
@@ -5,16 +5,25 @@ export default function useDarkmode() {
     window.matchMedia("(prefers-color-scheme: light)").matches ? false : true
   );
 
-  const DarkmodeButton = () => (
-    <button
-      type="button"
-      onClick={() => {
-        setIsDarkmode((prev) => !prev);
-      }}
-    >
-      {isDarkmode ? "Lightmode" : "Darkmode"}
-    </button>
-  );
+  const DarkmodeButton = ({
+    ...props
+  }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+    // Private props destructured to prevent override
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { children, type: _type, onClick: _onClick, ...rest } = props;
+    return (
+      <button
+        type="button"
+        onClick={() => {
+          setIsDarkmode((prev) => !prev);
+        }}
+        {...rest}
+      >
+        {isDarkmode ? "Lightmode" : "Darkmode"}
+        {children}
+      </button>
+    );
+  };
 
   return { isDarkmode, DarkmodeButton };
 }
