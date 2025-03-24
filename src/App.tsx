be74853296cc5922173abc0d8527df9ff37ce5c2
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-24 18:05:03 -0400

    Add comment

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import useDarkmode from "./hooks/useDarkmode";
 function App() {
   const { isDarkmode, setIsDarkmode } = useDarkmode();
 
+  // dark: tailwind classes will be applied to any element that has a parent with the dark class
+  // @custom-variant in index.css causes this behavior
   return (
     <div
       id="app-container"
