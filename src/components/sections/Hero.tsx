9e3873830b81d0ab65e7ce7d2c59c64f92f5fa72
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-24 19:54:01 -0400

    Add grid layout

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,6 @@
 import { SetStateAction } from "react";
 import DarkmodeButton from "../ui/DarkModeButton";
+import SectionLink from "../ui/SectionLink";
 
 type HeroProps = {
   isDarkmode: boolean;
@@ -9,12 +10,28 @@ type HeroProps = {
 export default function Hero({ ...props }: HeroProps) {
   const { isDarkmode, setIsDarkmode } = props;
   return (
-    <section>
-      <DarkmodeButton isDarkmode={isDarkmode} setIsDarkmode={setIsDarkmode} />
-      <h1>Headline</h1>
-      <h2>Subtext</h2>
-
-      <p>Hero</p>
+    <section className="h-screen grid">
+      <DarkmodeButton
+        darkmode={isDarkmode}
+        setDarkmode={setIsDarkmode}
+        className="m-4 row-start-1 col-start-1 justify-self-end z-10"
+      />
+      <div className="row-start-1 col-start-1 justify-self-center h-full max-w-6xl grid gap-4 grid-rows-[1fr_min-content_min-content_1fr] grid-cols-4">
+        <h2 className="text-2xl px-8 row-start-1 col-span-full self-end">
+          Hello, I'm Josh.
+        </h2>
+        <h1 className="text-4xl px-8 row-start-2 col-span-full">
+          I build modern web applications that solve real problems.
+        </h1>
+        <h2 className="text-2xl px-8 row-start-3 col-span-full">
+          From APIs to front-end interfaces and custom tools, I create fast,
+          practical solutions using TypeScript, React, and modern tooling.
+        </h2>
+        <nav className="px-8 py-10 row-start-4 col-span-full flex gap-10">
+          <SectionLink section="Projects" />
+          <SectionLink section="Contact" />
+        </nav>
+      </div>
     </section>
   );
 }
