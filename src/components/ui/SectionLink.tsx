39f4d843ef6e2d4c3bf58487de92dd7c70a8e2b6
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-24 18:38:09 -0400

    Add basic style to button

--- src/components/ui/SectionLink.tsx
+++ src/components/ui/SectionLink.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from "tailwind-merge";
+
 type SectionName =
   | "Hero"
   | "About"
@@ -14,14 +16,22 @@ type SectionLinkProps = Omit<
 };
 
 export default function SectionLink({ ...props }: SectionLinkProps) {
-  const { section, ...rest } = props;
+  const { section, className, ...rest } = props;
   const handleClick = () => {
     // Jump to section
     return true;
   };
 
   return (
-    <button type="button" onClick={handleClick} {...rest}>
+    <button
+      type="button"
+      onClick={handleClick}
+      className={twMerge(
+        "text-xl font-bold px-6 py-2 rounded-md bg-indigo-600 hover:bg-indigo-700 cursor-pointer transition-all ease-in",
+        className
+      )}
+      {...rest}
+    >
       {section}
     </button>
   );
