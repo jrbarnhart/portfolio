cf67b0c5c4357cbe4f247f5c2d445c6c7a6ab3e0
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-24 18:11:20 -0400

    Adjust type name for uniformity

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,16 +1,19 @@
 import { SetStateAction } from "react";
 import DarkmodeButton from "../ui/DarkModeButton";
 
-type HeroPropTypes = {
+type HeroProps = {
   isDarkmode: boolean;
   setIsDarkmode: React.Dispatch<SetStateAction<boolean>>;
 };
 
-export default function Hero({ ...props }: HeroPropTypes) {
+export default function Hero({ ...props }: HeroProps) {
   const { isDarkmode, setIsDarkmode } = props;
   return (
     <section>
       <DarkmodeButton isDarkmode={isDarkmode} setIsDarkmode={setIsDarkmode} />
+      <h1>Headline</h1>
+      <h2>Subtext</h2>
+
       <p>Hero</p>
     </section>
   );
