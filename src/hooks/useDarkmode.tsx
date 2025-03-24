8581b4d9ba036356bb6766f6f5505bccc3741187
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-24 16:54:18 -0400

    Use twmerge to construct className

--- src/hooks/useDarkmode.tsx
+++ src/hooks/useDarkmode.tsx
@@ -1,4 +1,6 @@
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function useDarkmode() {
   const [isDarkmode, setIsDarkmode] = useState(
@@ -8,18 +10,33 @@ export default function useDarkmode() {
   const DarkmodeButton = ({
     ...props
   }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-    // Private props destructured to prevent override
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { children, type: _type, onClick: _onClick, ...rest } = props;
+    const {
+      children,
+      className,
+      // Private props destructured to prevent override
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      type: _type,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      onClick: _onClick,
+      ...rest
+    } = props;
     return (
       <button
         type="button"
         onClick={() => {
           setIsDarkmode((prev) => !prev);
         }}
+        className={twMerge(
+          "px-4 py-2 bg-indigo-700 text-white rounded-sm w-16 flex justify-center",
+          className
+        )}
         {...rest}
       >
-        {isDarkmode ? "Lightmode" : "Darkmode"}
+        {isDarkmode ? (
+          <SunIcon className="h-6 w-6" />
+        ) : (
+          <MoonIcon className="h-6 w-6" />
+        )}
         {children}
       </button>
     );
