66f1715ebd4b8f05a4553e733b96b31641cf0dd7
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-25 01:06:59 -0400

    Add reactive text style and description

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,7 +1,16 @@
 export default function About() {
   return (
-    <section>
-      <p>About</p>
+    <section className="text-slate-950 dark:text-violet-50 px-8 max-w-6xl justify-self-center">
+      <h2 className="text-3xl md:text-5xl font-semibold">About Me</h2>
+      <p className="[text-shadow:_1px_1px_2px_var(--tw-shadow-color)] shadow-slate-500/50 dark:shadow-indigo-950 mt-4 text-xl md:text-2xl">
+        I’m a self-taught developer who loves solving real-world problems with
+        modern web tech. I have a genuine love for learning and am always
+        exploring new concepts.
+      </p>
+      <p className="[text-shadow:_1px_1px_2px_var(--tw-shadow-color)] shadow-slate-500/50 dark:shadow-indigo-950 mt-4 text-xl md:text-2xl">
+        Outside of web development my interests include storytelling and world
+        creation, game development, and machine learning.
+      </p>
     </section>
   );
 }
