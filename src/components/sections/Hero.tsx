f5ab1f3324a5b64c8d30593f78b32123df9d99eb
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-24 23:57:02 -0400

    Add text shadow for standout effect

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -17,13 +17,13 @@ export default function Hero({ ...props }: HeroProps) {
         className="m-4 row-start-1 col-start-1 justify-self-end z-10"
       />
       <div className="row-start-1 col-start-1 justify-self-center h-full max-w-6xl grid gap-4 grid-rows-[1fr_min-content_min-content_1fr] grid-cols-4">
-        <h2 className="text-3xl md:text-5xl font-bold px-8 row-start-1 col-span-full self-end">
+        <h2 className="[text-shadow:_1px_1px_5px_var(--tw-shadow-color)] shadow-indigo-500/50 dark:shadow-indigo-950 text-3xl md:text-5xl font-bold  px-8 row-start-1 col-span-full self-end">
           Hello, I'm Josh.
         </h2>
-        <h1 className="text-4xl md:text-6xl font-bold px-8 row-start-2 col-span-full">
+        <h1 className="[text-shadow:_1px_1px_5px_var(--tw-shadow-color)] shadow-indigo-500/50 dark:shadow-indigo-950 text-4xl md:text-6xl font-bold px-8 row-start-2 col-span-full">
           I build modern web applications that solve real problems.
         </h1>
-        <h2 className="text-2xl md:text-4xl font-semibold px-8 row-start-3 col-span-full">
+        <h2 className="[text-shadow:_1px_1px_5px_var(--tw-shadow-color)] shadow-indigo-500/50 dark:shadow-indigo-950 text-2xl md:text-4xl font-semibold px-8 row-start-3 col-span-full">
           From APIs to front-end interfaces and custom tools, I create fast,
           practical solutions using TypeScript, React, and modern tooling.
         </h2>
